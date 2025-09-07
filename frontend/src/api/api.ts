f206736b4d35ef0d2e46298880f0357bd8197cec
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-09-07 21:26:28 +0300

    refactor: added async/await in api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,23 +4,28 @@ import type { ITableData } from '../types/tableData.interface.ts'
 
 const BASE_URL = 'http://127.0.0.1:8000/api'
 
-export const fetchChartData = async (eventType: string | undefined) => {
-	return await axios
-		.get<IChartData>(
+export const fetchChartData = async (eventType?: string) => {
+	try {
+		const res = await axios.get<IChartData>(
 			`${BASE_URL}/chart${eventType ? `?event_type=${eventType}` : ''}`
 		)
-		.then(res => res.data)
+		return res.data
+	} catch (e) {
+		console.error(e)
+		throw e
+	}
 }
 
-export const fetchTableData = async (
-	eventType: string | undefined,
-	groupBy: string | undefined
-) => {
-	return await axios
-		.get<ITableData[]>(
+export const fetchTableData = async (eventType?: string, groupBy?: string) => {
+	try {
+		const res = await axios.get<ITableData[]>(
 			`${BASE_URL}/table?${groupBy ? `group_by=${groupBy}` : ''}${
 				eventType ? `&event_type=${eventType}` : ''
 			}`
 		)
-		.then(res => res.data)
+		return res.data
+	} catch (e) {
+		console.error(e)
+		throw e
+	}
 }
